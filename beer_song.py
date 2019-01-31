0dfa034d520020dd1b40fd23f9d4f5ad0ffc1b20
dorisgjata <dorisgjata@oakland.edu>
2019-01-30 20:14:28 -0500

    Implemented beer function

--- beer_song.py
+++ beer_song.py
@@ -1,6 +1,14 @@
 def main():
     ninety_nine_bottles_of_beer()
 def ninety_nine_bottles_of_beer():
-    print("wasted")
+  for bottles in range(99, 1, -1):
+        if bottles == 1:
+            print( "1 bottle of	beer on the wall, 1 bottle of beer")
+	        print(bootles-1, "take one down, pass it around, no more bottles of beer on the	wall")
+
+        else:
+            print(bootles, "%d bottles of beer on the wall, %d bottles of beer")
+            print(bootles-1, "take one down, pass it around, %d bottles of beer on the wall")
+
 if __name__ == "__main__":
     main()
