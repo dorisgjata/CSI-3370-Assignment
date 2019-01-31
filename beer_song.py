b7b4d1825723d5d48a7e21213c98e8163f27f71e
dorisgjata <dorisgjata@oakland.edu>
2019-01-30 20:25:36 -0500

    Fixed beer song

--- beer_song.py
+++ beer_song.py
@@ -1,14 +1,14 @@
 def main():
     ninety_nine_bottles_of_beer()
 def ninety_nine_bottles_of_beer():
-  for bottles in range(99, 1, -1):
+  for bottles in range(99, 0, -1):
         if bottles == 1:
-            print( "1 bottle of	beer on the wall, 1 bottle of beer")
-	        print(bootles-1, "take one down, pass it around, no more bottles of beer on the	wall")
+            print("1 bottle of beer on the wall, 1 bottle of beer")
+            print("take one down, pass it around, no more bottles of beer on the	wall")
 
         else:
-            print(bootles, "%d bottles of beer on the wall, %d bottles of beer")
-            print(bootles-1, "take one down, pass it around, %d bottles of beer on the wall")
-
+            print("%d bottles of beer on the wall, %d bottles of beer"% (bottles,bottles) )
+            down=bottles-1
+            print("take one down, pass it around, %d bottles of beer on the wall"% ((down)))
 if __name__ == "__main__":
     main()
